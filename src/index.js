d99e348d60ff89db40db1dad38c926b01a9fca87
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2022-01-13 20:05:20 +0200

    fetch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import './css/styles.css';
 
+import debounce from 'lodash.debounce';
+
+import Notiflix from 'notiflix';
+
+import './fetch';
+
+import './renderMarkUp';
 const DEBOUNCE_DELAY = 300;
+
+let refs = {
+  input: document.querySelector('#search-box'),
+  country: document.querySelector('.country-list'),
+  info: document.querySelector('.country-info'),
+};
+
+refs.input.addEventListener('input', debounce(inputCountry, DEBOUNCE_DELAY));
+
+function inputCountry(e) {
+  if (e.target.value.length > 1 && e.target.value.length <= 10) {
+  }
+}
